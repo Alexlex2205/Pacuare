c508f1ec97428076914b7a588fb55ed98cd1cb06
Your Name <you@example.com>
2026-05-25 08:16:34 -0600

    cambios

--- app/src/main/java/com/example/pacuare/MainActivity.kt
+++ app/src/main/java/com/example/pacuare/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.pacuare
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val usuario = findViewById<EditText>(R.id.user)
+        val contra = findViewById<EditText>(R.id.password)
+
+        val imglogin = findViewById<ImageView>(R.id.btnlog)
+        val txtlogin = findViewById<TextView>(R.id.btnlogtxt)
+
+
     }
 }
 
